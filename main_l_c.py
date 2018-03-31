c7106e17782f24eee73066fa02b769bed57cdabd
etilawin <kimik812@gmail.com>
2018-03-31 20:06:31 +0200

    Finally really working

--- main_l_c.py
+++ main_l_c.py
@@ -1,6 +1,5 @@
 import random
 import copy
-import os
 
 W = 400
 H = 400
@@ -27,16 +26,16 @@ def initialiser():
     Retourne : Rien
     C.U : Aucune
     """
-    for colonne in range(W):
+    for ligne in range(H):
         tableau_cellules.append([])
-        for ligne in range(H):
+        for colonne in range(W):
             # Création de la cellule
             if ligne == H / 2 and colonne == W / 2:
                 cellule = [True, 0, 1, 0]
-                cristal.add((colonne, ligne))
+                cristal.add((ligne, colonne))
             else:
                 cellule = [False, 0, 0, RHO]
-            tableau_cellules[colonne].append(cellule)
+            tableau_cellules[ligne].append(cellule)
             # Coordonées des voisins
             # Lignes...
             # ...Pairs :
@@ -48,25 +47,25 @@ def initialiser():
             # 1 x 1
             # 0 1 1
             if ligne % 2 == 0:
-                voisins[colonne, ligne] = [(colonne - 1, ligne - 1),
-                                           (colonne - 1, ligne),
-                                           (colonne - 1, ligne + 1),
-                                           (colonne, ligne - 1),
-                                           (colonne, ligne + 1),
-                                           (colonne + 1, ligne)]
+                voisins[ligne, colonne] = [(ligne - 1, colonne - 1),
+                                           (ligne - 1, colonne),
+                                           (ligne, colonne - 1),
+                                           (ligne, colonne + 1),
+                                           (ligne + 1, colonne - 1),
+                                           (ligne + 1, colonne)]
             else:
-                voisins[colonne, ligne] = [(colonne - 1, ligne),
-                                           (colonne, ligne - 1),
-                                           (colonne, ligne + 1),
-                                           (colonne + 1, ligne - 1),
-                                           (colonne + 1, ligne),
-                                           (colonne + 1, ligne + 1)]
+                voisins[ligne, colonne] = [(ligne - 1, colonne),
+                                           (ligne - 1, colonne + 1),
+                                           (ligne, colonne - 1),
+                                           (ligne, colonne + 1),
+                                           (ligne + 1, colonne),
+                                           (ligne + 1, colonne + 1)]
 
-            voisins[colonne, ligne] = list(filter(lambda x: (x[0] != W and
-                                                             x[1] != H and
-                                                             x[0] != -1 and
-                                                             x[1] != -1),
-                                                  voisins[colonne, ligne]))
+            voisins[ligne, colonne] = list(filter(lambda x: (x[1] != W and
+                                               x[0] != H and
+                                               x[0] != -1 and
+                                               x[1] != -1),
+                                    voisins[ligne, colonne]))
 
 
 def get_cel_voisin(cellules_voisines, n=-1):
@@ -77,12 +76,12 @@ def get_cel_voisin(cellules_voisines, n=-1):
     C.U : None
     """
     if 0 <= n < 5:
-        return [tableau_cellules[colonne][ligne][n] for (colonne, ligne) in cellules_voisines]
+        return [tableau_cellules[ligne][colonne][n] for (ligne, colonne) in cellules_voisines]
     else:
-        return [tableau_cellules[colonne][ligne] for (colonne, ligne) in cellules_voisines]
+        return [tableau_cellules[ligne][colonne] for (ligne, colonne) in cellules_voisines]
 
 
-def diffusion(cel, cel_up, voisins_cel):
+def diffusion(cel_up, voisins_cel):
     """
     Fonction qui effectue la diffusion de la vapeur d'une cellule sur ses voisins
     paramètre ligne : (int) la ligne de la cellule concernée
@@ -90,7 +89,7 @@ def diffusion(cel, cel_up, voisins_cel):
     retourne : None
     C.U : None
     """
-    if (colonne, ligne) not in frontiere:
+    if (ligne, colonne) not in frontiere:
         v = get_cel_voisin(voisins_cel, 3)
         moyenne = (cel[3] + sum(v)) / (len(v) + 1)
     else:
@@ -127,7 +126,7 @@ def attachement(cel_up, voisins_cel):
     retourne : None
     C.U : None
     """
-    voisins_du_cristal = len([x for x in get_cel_voisin(voisins_cel) if x[0]])
+    voisins_du_cristal = len([x for x in voisins_cel if x in cristal])
     in_cristal = False
     if voisins_du_cristal <= 2:
         if cel_up[1] > BETA:
@@ -145,11 +144,11 @@ def attachement(cel_up, voisins_cel):
         cel_up[2] = cel_up[2] + cel_up[1]
         cel_up[1] = cel_up[3] = 0
         cel_up[0] = True
-        cristal.add((colonne, ligne))
-        frontiere_up.remove((colonne, ligne))
-        for colonne1, ligne1 in voisins[colonne, ligne]:
-            if (colonne1, ligne1) not in cristal:
-                frontiere_up.add((colonne1, ligne1))
+        cristal.add((ligne, colonne))
+        frontiere_up.remove((ligne, colonne))
+        for ligne1, colonne1 in voisins[ligne, colonne]:
+            if (ligne1, colonne1) not in cristal:
+                frontiere_up.add((ligne1, colonne1))
 
 
 def fonte(cel_up):
@@ -176,15 +175,6 @@ def bruit(cel_up):
     i = random.randint(-1, 1)
     cel_up[3] *= 1 + i * SIGMA
 
-def create_folder():
-    """ Fonction qui créer un dossier pour stocker les images """
-    base = "images_"
-    i=0
-    while os.path.exists(base + str(i)):
-        i += 1
-    os.makedirs(base + str(i))
-    return base + str(i)
-
 
 if __name__ == "__main__":
     from PIL import Image
@@ -194,33 +184,32 @@ if __name__ == "__main__":
     flocon = Image.new('RGB', (W, H))
 
     px = flocon.load()
-    frontiere = set(x for x in voisins[W / 2, H / 2])
-
-    chemin = create_folder()
+    frontiere = set(x for x in voisins[H / 2, W / 2])
 
     for i in range(3300):
         updated_tableau = copy.deepcopy(tableau_cellules)
         frontiere_up = frontiere.copy()
-        for colonne in range(W):
-            for ligne in range(H):
-                cel = tableau_cellules[colonne][ligne]
-                cel_up = updated_tableau[colonne][ligne]
-                voisins_cel = voisins[colonne, ligne]
-                if not cel[0]:
-                    diffusion(cel, cel_up, voisins_cel)
+        for ligne in range(H):
+            for colonne in range(W):
+                cel = tableau_cellules[ligne][colonne]
+                cel_up = updated_tableau[ligne][colonne]
+                voisins_cel = voisins[ligne, colonne]
+                diffusion(cel_up, voisins_cel)
 
-        for (colonne, ligne) in frontiere:
-            cel_up = updated_tableau[colonne][ligne]
-            voisins_cel = voisins[colonne, ligne]
+        for (ligne, colonne) in frontiere:
+            cel_up = updated_tableau[ligne][colonne]
+            voisins_cel = voisins[ligne, colonne]
             gel(cel_up)
             attachement(cel_up, voisins_cel)
             if not cel_up[0]:
                 fonte(cel_up)
                 bruit(cel_up)
-            px[colonne, ligne] = (i % 256, (i * i) % 256, (255 - i) % 256)
 
         frontiere = frontiere_up.copy()
         tableau_cellules = copy.deepcopy(updated_tableau)
 
+        for (ligne, colonne) in frontiere:
+            px[colonne, ligne] = (i % 256, (i * i) % 256, (255 - i) % 256)
+
         if i % 25 == 0:
-            flocon.save(os.path.join(chemin, "image" + str(i//25) + ".jpg"))
+            flocon.show()
