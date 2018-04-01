c195f0d1bc47ff10765c3396a5799b6cfda5a265
etilawin <kimik812@gmail.com>
2018-04-01 21:37:46 +0200

    enfin un pavage réussi ...

--- graphisme.py
+++ graphisme.py
@@ -1,12 +1,14 @@
 from math import pi, sin
 from PIL import Image, ImageDraw
 
+
 def _mettre_a_lechelle(generateur, width, height, taille=50):
     height_echelle = int(height / taille) + 2
     width_echelle = int(width / taille) + 2
 
     for coordonnees in generateur(width_echelle, height_echelle):
         yield [(x * taille, y * taille) for (x, y) in coordonnees]
+
 
 def generer_unitee_hexagonal(width, height):
     """
@@ -21,7 +23,7 @@ def generer_unitee_hexagonal(width, height):
     #                     (x, y) +-----+ (x + 1, y)
     #                           /       \
     #                          /         \
-    #         (x - 1/2 y + h) +           + (x + 3/2, y + h)
+    #        (x - 1/2, y + h) +           + (x + 3/2, y + h)
     #                          \         /
     #                           \       /
     #                (x, y + 2h) +-----+ (x + 1, y + 2h)
@@ -41,34 +43,58 @@ def generer_unitee_hexagonal(width, height):
     # There are offsets to ensure we fill the entire canvas.
 
     # Moitié de la taille de l'hexagone
-    h = sin(pi / 3)
+    l = sin(pi / 3)
+    colonne = 0
+    for x in range(-1, int(width / l) + 1):
+        ligne = 0 if (x % 2 == 1) else 1
+        for y in range(-1, height, 3):
 
-    for x in range(-1, width, 3):
-        for y in range(-1, int(height / h) + 1):
+            y_ = y if (x % 2 == 0) else y + 1.5
 
-            # On shift 1 ligne sur 2 pour obtenir quelque chose comme :
-            #
-            #         /‾‾‾\   /‾‾‾\   /‾‾‾\
-            #         \___/‾‾‾\___/‾‾‾\___/‾‾‾\
-            #             \___/   \___/   \___/
-            #
-            x_ = x if (y % 2 == 0) else x + 1.5
+            color = (0,0,0)
+            if (colonne,ligne) in cristal:
+                color = (0,0,255)
+
+            ligne += 2
 
             yield [
-                (x_,        y * h),
-                (x_ + 1,    y * h),
-                (x_ + 1.5, (y + 1) * h),
-                (x_ + 1,   (y + 2) * h),
-                (x_,       (y + 2) * h),
-                (x_ - 0.5, (y + 1) * h),
+                (x * l,          y_),
+                (x * l,          y_ + 1),
+                ((x + 1) * l,    y_ + 1.5),
+                ((x + 2) * l,    y_ + 1),
+                ((x + 2) * l,    y_),
+                ((x + 1) * l,    y_ - 0.5),
             ]
+        colonne = colonne + 1 if (x % 2 == 0) else colonne
+
+    # for x in range(-1, width, 3):
+    #     for y in range(-1, int(height / h) + 1):
+    #
+    #         # On shift 1 ligne sur 2 pour obtenir quelque chose comme :
+    #         #
+    #         #         /‾‾‾\   /‾‾‾\   /‾‾‾\
+    #         #         \___/‾‾‾\___/‾‾‾\___/‾‾‾\
+    #         #             \___/   \___/   \___/
+    #         #
+    #         x_ = x if (y % 2 == 0) else x + 1.5
+    #
+    #         yield [
+    #             (x_,        y * h),
+    #             (x_ + 1,    y * h),
+    #             (x_ + 1.5, (y + 1) * h),
+    #             (x_ + 1,   (y + 2) * h),
+    #             (x_,       (y + 2) * h),
+    #             (x_ - 0.5, (y + 1) * h)
+    #         ]
+
 
 def generer_hexagones(*args, **kwargs):
     """Créer une grille hexagonale"""
     return _mettre_a_lechelle(generer_unitee_hexagonal, *args, **kwargs)
 
+
 if __name__ == "__main__":
-    im = Image.new(mode="RGB", size=(800,800), color=0)
+    im = Image.new(mode="RGB", size=(800, 800), color=0)
     for coords in generer_hexagones(800, 800, taille=50):
         ImageDraw.Draw(im).polygon(coords, outline="black", fill="white")
     im.show()
--- main.py
+++ main.py
@@ -3,10 +3,18 @@ import copy
 import os
 # from pycallgraph import PyCallGraph
 # from pycallgraph.output import GraphvizOutput
-from graphisme import *
+from math import pi, sin
+from PIL import Image, ImageDraw
 
-W = 400
-H = 400
+W = 2000
+H = 2000
+
+T_HEXAGONES = 5
+H_TABLEAU = int(H / (T_HEXAGONES * (1 + sin(pi / 3)))) + 2
+W_TABLEAU = int(W / (T_HEXAGONES + 1.5) + 2)
+
+H_TABLEAU = H_TABLEAU if H_TABLEAU % 2 == 0 else H_TABLEAU + 1
+W_TABLEAU = W_TABLEAU if W_TABLEAU % 2 == 0 else W_TABLEAU + 1
 
 KAPPA = 0.6
 BETA = 0.65
@@ -16,7 +24,7 @@ RHO = 1.1
 MU = 0.5
 GAMMA = 0.5
 
-SIGMA = 0.005
+SIGMA = 0.000
 
 tableau_cellules = []
 voisins = {}
@@ -31,12 +39,12 @@ def initialiser():
     C.U : Aucune
     """
     append = tableau_cellules.append
-    for colonne in range(W):
+    for colonne in range(W_TABLEAU):
         append([])
         add_ligne = tableau_cellules[colonne].append
-        for ligne in range(H):
+        for ligne in range(H_TABLEAU):
             # Création de la cellule
-            if ligne == H / 2 and colonne == W / 2:
+            if ligne == H_TABLEAU / 2 and colonne == W_TABLEAU / 2:
                 cellule = [True, 0, 1, 0]
                 cristal.add((colonne, ligne))
             else:
@@ -67,8 +75,8 @@ def initialiser():
                                            (colonne + 1, ligne),
                                            (colonne + 1, ligne + 1)]
 
-            voisins[colonne, ligne] = list(filter(lambda x: (x[0] != W and
-                                                             x[1] != H and
+            voisins[colonne, ligne] = list(filter(lambda x: (x[0] != W_TABLEAU and
+                                                             x[1] != H_TABLEAU and
                                                              x[0] != -1 and
                                                              x[1] != -1),
                                                   voisins[colonne, ligne]))
@@ -182,47 +190,93 @@ def bruit(cel_up):
     i = random.randint(-1, 1)
     cel_up[3] *= 1 + i * SIGMA
 
+
 def create_folder():
     """ Fonction qui créer un dossier pour stocker les images """
     base = "images_"
-    i=0
+    i = 0
     while os.path.exists(base + str(i)):
         i += 1
     os.makedirs(base + str(i))
     return base + str(i)
 
 
+def _mettre_a_lechelle(generateur, width, height, taille=50):
+    height_echelle = int(height / taille) + 2
+    width_echelle = int(width / taille) + 2
+
+    for coordonnees in generateur(width_echelle, height_echelle):
+        yield ([(x * taille, y * taille) for (x, y) in coordonnees[:-1]], coordonnees[-1])
+
+
+def generer_unitee_hexagonal(width, height):
+    """
+    Créer les coordinnées d'un hexagones pour ensuite le passer à _mettre_a_lechelle puis à draw.polygon
+    """
+    # Moitié de la taille de l'hexagone
+    l = sin(pi / 3)
+    colonne = 0
+    for x in range(0, int(width / l) + 1):
+        ligne = 0 if (x % 2 == 1) else 1
+        for y in range(0, height, 3):
+
+            y_ = y if (x % 2 == 0) else y + 1.5
+
+            color = (255,255,255)
+            if (colonne,ligne) in cristal:
+                color = (0,0,255)
+
+            ligne += 2
+
+            yield [
+                (x * l,          y_),
+                (x * l,          y_ + 1),
+                ((x + 1) * l,    y_ + 1.5),
+                ((x + 2) * l,    y_ + 1),
+                ((x + 2) * l,    y_),
+                ((x + 1) * l,    y_ - 0.5),
+                color,
+            ]
+        if x % 2 == 0:
+            colonne += 1
+
+
+def generer_hexagones(*args, **kwargs):
+    """Créer une grille hexagonale"""
+    return _mettre_a_lechelle(generer_unitee_hexagonal, *args, **kwargs)
+
+
 if __name__ == '__main__':
-    from PIL import Image
 
     initialiser()
 
     flocon = Image.new('RGB', (W, H))
 
     px = flocon.load()
-    frontiere = set(x for x in voisins[W / 2, H / 2])
+    frontiere = set(x for x in voisins[W_TABLEAU / 2, H_TABLEAU / 2])
 
     chemin = create_folder()
 
-    dc = copy.deepcopy # Augmente la vitesse d'execution (https://wiki.python.org/moin/PythonSpeed/PerformanceTips)
+    # Augmente la vitesse d'execution (https://wiki.python.org/moin/PythonSpeed/PerformanceTips)
+    dc = copy.deepcopy
     join = os.path.join
 
     all_possibilities = []
     add_to_all = all_possibilities.append
-    for colonne in range(W):
-        for ligne in range(H):
+    for colonne in range(W_TABLEAU):
+        for ligne in range(H_TABLEAU):
             add_to_all((colonne, ligne))
 
     for i in range(3300):
         updated_tableau = dc(tableau_cellules)
         frontiere_up = frontiere.copy()
 
-        for (colonne, ligne) in all_possibilities: # Évite de recréer à chaque fois un range
-                cel = tableau_cellules[colonne][ligne]
-                if not cel[0]:
-                    cel_up = updated_tableau[colonne][ligne]
-                    voisins_cel = voisins[colonne, ligne]
-                    diffusion(cel, cel_up, voisins_cel)
+        for (colonne, ligne) in all_possibilities:  # Évite de recréer à chaque fois un range
+            cel = tableau_cellules[colonne][ligne]
+            if not cel[0]:
+                cel_up = updated_tableau[colonne][ligne]
+                voisins_cel = voisins[colonne, ligne]
+                diffusion(cel, cel_up, voisins_cel)
 
         for (colonne, ligne) in frontiere:
             cel_up = updated_tableau[colonne][ligne]
@@ -232,10 +286,14 @@ if __name__ == '__main__':
             if not cel_up[0]:
                 fonte(cel_up)
                 bruit(cel_up)
-            px[colonne, ligne] = (i % 256, (i * i) % 256, (255 - i) % 256)
+            # px[colonne, ligne] = (i % 256, (i * i) % 256, (255 - i) % 256)
 
         frontiere = frontiere_up.copy()
         tableau_cellules = dc(updated_tableau)
 
         if i % 25 == 0:
-            flocon.save(join(chemin, "image" + str(i//25) + ".png"))
+            for infos in generer_hexagones(W, H, taille=T_HEXAGONES):
+                shape = infos[0]
+                color = infos[-1]
+                ImageDraw.Draw(flocon).polygon(shape, fill=color)
+            flocon.save(join(chemin, "image" + str(i // 25) + ".png"))
